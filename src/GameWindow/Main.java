0431358e59868937c11d95659558d4c571fa398e
MyYoLo <1797415750@qq.com>
2018-10-22 16:49:38 +0800

    注释语句

---
+++ src/GameWindow/Main.java
@@ -0,0 +1,9 @@
+
+package GameWindow;
+public class Main {
+
+    public static void main(String[] args) {
+
+        GameWindow gm = new GameWindow();
+    }
+}
---
+++ src/GameWindow/PuzzlePanel.java
@@ -0,0 +1,9 @@
+package GameWindow;
+import javax.swing.*;
+import java.awt.*;
+
+public class PuzzlePanel extends JPanel {
+    public PuzzlePanel() {
+        setBackground(Color.blue);
+    }
+}
